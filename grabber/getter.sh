9ae465cf43e66aa698a45dcc8b37d4d5238e35e4
nickfyson <nickfyson@github.com>
2025-01-13 21:49:55 +0000

    iterate on putter action

---
+++ grabber/getter.sh
@@ -0,0 +1,33 @@
+set -e
+
+# process arguments to the script, check exactly two arguments are passed
+if [ $# -ne 3 ]; then
+    echo "Usage: $0 <gist-url> <gist-filename> <write-path>"
+    exit 1
+fi
+
+GIST_URL=$1
+GIST_FILENAME=$2
+DIRECTORY=$3
+
+# ensure directory exists
+mkdir -p $DIRECTORY
+
+# generate path for temp file
+ZIPBASE64=$(mktemp)
+
+echo "ZIPBASE64: $ZIPBASE64"
+
+gh gist view $GIST_URL --filename $GIST_FILENAME > $ZIPBASE64
+
+ZIP=$(mktemp)
+echo "ZIP: $ZIP"
+
+# decode the base64 encoded file
+base64 -d -i $ZIPBASE64 -o $ZIP
+
+# extract the zip file
+unzip $ZIP -d $DIRECTORY
+
+# list content of the directory recursively
+ls -R $DIRECTORY
